80132cbd12ccfd960ed1abae1dfcb7aeb156a32e
fulian23 <3296544069@qq.com>
2025-04-09 22:45:54 +0800

    新增测试路由

--- run.py
+++ run.py
@@ -13,8 +13,12 @@ def hello_world():
     data = {'name': 'John', 'age': 25, "list":['a','b','c']}
     return render_template('base.html',data=data)
 
+@app.route('/test')
+def test():
+    data={} #这里填需要的测试数据
+    return render_template('test.html',data=data)
+
 
 if __name__ == '__main__':
     app.run()
-    print(111)
 
